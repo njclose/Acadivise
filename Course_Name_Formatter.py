bd7f67ca2b138560a5ad4bf3d3b2d19b245f2f63
niclUCB <nicl1251@colorado.edu>
2024-12-02 13:56:12 -0700

    Completed CU Boulder Class List

--- Course_Name_Formatter.py
+++ Course_Name_Formatter.py
@@ -1,42 +1,35 @@
-#Directory: C:\Users\njclo\OneDrive\Documents\GitHub\College-Program-Helper\CU_Boulder_Class_Files
-
 import re
+from pathlib import Path
 from pdfquery import PDFQuery
 
-# Prompt for file path
-print('What is the file path?: ')
-pdfPath = input()
-print(f"Provided file path: {pdfPath}")
-
-# Load the PDF
-pdf = PDFQuery(pdfPath)
-pdf.load()
-
-# Extract text elements
-text_elements = pdf.pq('LTTextLineHorizontal')
-
-# Compile regex pattern
-pattern = re.compile(r"([A-Z]{4}\s\d{4})\s\((\d+)\)\s(.+?)")
-
-# Iterate over text elements and apply regex
-for element in text_elements:
-    text = element.get('text')  # Access text attribute of LayoutElement
-    print(text)
-    #if text:  # Ensure there's text to process
-        #matches = pattern.findall(text)
-        #for match in matches:
-            #print(match)
+# ADD THE ABILITY TO TYPE IN SEPERATE FILE PATHS AND OUTPUT FILES
 
 
-#for text_element in text_elements:
-#    font_name = text_element.get('fontname', '')
-#    if 'Bold' in font_name and 'Roboto' in font_name:
-#        text = text_element.text
-#        #if courseAbr in text:
-#        for char in text:
-#           if char == ' ':
-#               char = ','
-#           elif char == '(' or char == ')':
-#               char = ' '
-#
-#        print(text)
+folder_path = Path(r"..\College-Program-Helper\CU_Boulder_Class_Files")
+
+with open(r"..\College-Program-Helper\College_Information\Courses_(CU).txt", 'w', encoding='utf-8') as file:
+    for file_path in folder_path.rglob("*"):  # Use glob for specific patterns, rglob for all files
+        if file_path.is_file():
+
+            # Load the PDF
+            pdf = PDFQuery(file_path)
+            pdf.load()
+
+            # Extract all LTTextLineHorizontal elements
+            text_elements = pdf.pq('LTTextLineHorizontal')
+
+            # Concatenate all the text content into a single string
+            full_text = " ".join([element.text for element in text_elements if element.text])
+
+            # Compile regex pattern
+            pattern = re.compile(r"([A-Z]{4}\s\d{4})\s\((\d+|\d+[-]\d+|\d+[.]\d+)\)\s(.+?(?=  ))")
+
+            # Prints out matches
+            
+            matches = pattern.findall(full_text)
+            for match in matches:
+                fixedMatch = ", ".join(match) + "\n"
+                file.write(fixedMatch)
+                        #print(match)
+
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv){
+
+    return 0;
+}
