05b97a67d553068a8625ecff3f8fda8958b3aa3b
niclUCB <nicl1251@colorado.edu>
2025-04-15 23:11:51 -0600

    Finished navbar & Modified Color Palette

--- Frontend/website/src/App.js
+++ Frontend/website/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css'; // Optional global styles
+import logo from './image_resources/acadivise_logo_textRightImage.png';
 
 // Page components
 import Home from './Pages/Home.js';
@@ -11,7 +12,9 @@ function App() {
   return (
     <Router>
       <nav className="navbar">
-        <Link to="/" className="routerLink">Acadivise</Link>
+        <Link to="/" className="logoLink">
+          <img src={logo} alt="Acadivise Logo" className="logo"></img>
+        </Link>
         <Link to="/schools" className="routerLink">Schools</Link>
         <Link to="/about" className="routerLink">About</Link>
       </nav>
